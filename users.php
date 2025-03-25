4f3949a2be7747da6a086e504e0925b8ae25968e
Sandrakyla <churilin_10@mail.ru>
2025-03-25 19:05:12 +0300

    Adding user authentication capability

---
+++ users.php
@@ -0,0 +1,14 @@
+<?php
+// Данные пользователей (логин => хэш пароля + доп. данные)
+return [
+    'admin' => [
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'birthday' => '1990-01-01',
+        'name' => 'Администратор'
+    ],
+    'client' => [
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'birthday' => '2000-05-15',
+        'name' => 'Иван Иванов'
+    ]
+];
